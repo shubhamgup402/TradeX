dc40abb38072f5daaeef43c46313cae3c5a0db76
Shubham Gupta <shubhamgup402@gmail.com>
2024-09-25 23:03:14 +0530

    recommit

--- db/config.js
+++ db/config.js
@@ -1,29 +1,29 @@
-// // config.js
-// const { Pool } = require('pg');
-
-// const pool = new Pool({
-//   host: "aws-0-ap-south-1.pooler.supabase.com",
-//   port: 6543,
-//   user: "postgres.ucshelblvordxyrvpxdt",
-//   password: "blogger@123$#",
-//   database: "postgres",
-//   ssl: {
-//     rejectUnauthorized: false
-//   }
-// });
-
-// module.exports = pool;
-
-
-// config.j s
+// config.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  host: "localhost",
-  port: 5432,
-  user: "postgres",
+  host: "aws-0-ap-south-1.pooler.supabase.com",
+  port: 6543,
+  user: "postgres.ucshelblvordxyrvpxdt",
   password: "blogger@123$#",
-  database: "virtualdb",
+  database: "postgres",
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 module.exports = pool;
+
+
+// // config.j s
+// const { Pool } = require('pg');
+
+// const pool = new Pool({
+//   host: "localhost",
+//   port: 5432,
+//   user: "postgres",
+//   password: "blogger@123$#",
+//   database: "virtualdb",
+// });
+
+// module.exports = pool;
